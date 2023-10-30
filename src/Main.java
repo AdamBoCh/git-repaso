b045ecb0a76e96f2d69d44ff36b01491e8a55216
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:57:23 +0100

    He añadido codigo nuevo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Its my first code!");
+        for (int i = 0;i < 0;i++){
+            System.out.println("X: ");
+        }
     }
 }
