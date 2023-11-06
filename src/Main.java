6922db684ea704e572264f5dd35a77f399bf8598
ik012982i9 <amurciaqu23dw@ikzubirimanteo.com>
2023-11-06 12:35:16 +0100

    He añadido lineas de codigo

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Its my first code!");
-        for (int i = 0;i < 0;i++){
-            System.out.println("X: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Numero: ");
+        int n = scanner.nextInt();
+        for (int i = 0;i < n;i++){
+            System.out.println("nuemros anteriores" + i);
         }
     }
 }
